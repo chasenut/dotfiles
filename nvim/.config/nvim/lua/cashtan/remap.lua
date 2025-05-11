a7e9f5ec95aaa34fc51f21ac5e19fd73aac38a76
cashtann <switalamaciej5@gmail.com>
2025-05-11 14:39:31 +0200

    new remaps

--- nvim/.config/nvim/lua/cashtan/remap.lua
+++ nvim/.config/nvim/lua/cashtan/remap.lua
@@ -1,12 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move highlighted code up & down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 
-local harpoon=require("harpoon")
-local mark=require("harpoon.mark")
-local ui=require("harpoon.ui")
 
+
+-- Harpoon
 vim.keymap.set("n", "<leader>a", function() require("harpoon.mark").add_file() end)
 vim.keymap.set("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end)
 vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end)
